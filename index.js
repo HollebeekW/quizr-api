59a8c85ce86a9faf25528e4c47218906d806ba40
HollebeekW <124771925+HollebeekW@users.noreply.github.com>
2026-03-12 09:55:03 +0100

    set up database connection with Sequelize and create initial SQLite database file

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.get('/status', (req, res) => {
     });
 });
 
+// Database connection
+const sequelize = require('./database/database');
+
+sequelize.sync();
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
