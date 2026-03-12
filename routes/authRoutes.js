6d2d135f8ee249538c765459b45580feb477e8cc
HollebeekW <124771925+HollebeekW@users.noreply.github.com>
2026-03-12 10:58:38 +0100

    feat: add signup validation and update routes for user registration

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const authController = require('../controllers/authController');
+const { validateSignup } = require('../validation/authValidation');
 
 // Registration route
-router.post('/signup', authController.register);
+router.post('/signup', validateSignup, authController.register);
 
 module.exports = router;
