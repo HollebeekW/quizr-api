c3ecd0022a97909c231d88f3c2ab6bcd8bee8f4f
HollebeekW <124771925+HollebeekW@users.noreply.github.com>
2026-03-16 17:12:03 +0100

    feat: add getUserById endpoint to retrieve user by ID

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,10 +5,21 @@ const { sequelize, User } = require('../database/database');
 // GET users
 exports.getUsers = async (req, res) => {
     try {
-        const users = await User.findAll({
-            attributes: ['username', 'avatar_url']
-        })
+        const users = await User.findAll();
         res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// GET user by id
+exports.getUserById = async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
